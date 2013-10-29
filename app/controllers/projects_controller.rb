ce5fca45a6a3e4a4a396427a9eb3887f12de9ea8
Steve Richert <steve.richert@gmail.com>
2013-10-29 12:04:26 -0400

    Enable project registration

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,25 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :harvest_id, :guaranteed_hours)
+  end
+end
---
+++ spec/features/project_registration_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+feature "Project Registration" do
+  scenario "A user can register a project" do
+    visit root_path
+    click_link "Add Project"
+
+    expect {
+      fill_in "Name", with: "Project X"
+      fill_in "Harvest ID", with: "12345"
+      fill_in "Guaranteed Hours", with: "123"
+      click_button "Save"
+    }.to change {
+      Project.count
+    }.from(0).to(1)
+
+    project = Project.last
+    expect(project.name).to eq("Project X")
+    expect(project.harvest_id).to eq(12345)
+    expect(project.guaranteed_hours).to eq(123)
+  end
+end
