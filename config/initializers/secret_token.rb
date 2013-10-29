05802985f896b7ef87d2a904222bac38d13f4df9
Steve Richert <steve.richert@gmail.com>
2013-10-29 11:21:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hourglass::Application.config.secret_key_base = 'bc9228fe5a830e67742a16faa53e6d7a83244f1e01a610fde60c19ddbc4430c64e1a6d39967b18b6a30c573b820b2969e3c856924a5edeba543a384fdd2a248f'
