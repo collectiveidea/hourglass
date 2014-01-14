637ade697203189fc40784987361fcb2d8daab5a
Daniel Morrison <daniel@collectiveidea.com>
2014-01-14 10:59:54 -0500

    Use day of the week in statusboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def friendly_weekday(date)
+    if date == Date.current
+      "Today"
+    elsif date == Date.current - 1
+      "Yesterday"
+    else
+      date.strftime('%A')
+    end
+  end
 end
--- app/views/date_totals/_date_total.json.jbuilder
+++ app/views/date_totals/_date_total.json.jbuilder
@@ -1,2 +1,2 @@
-json.title date_total.date
+json.title friendly_weekday(date_total.date)
 json.value date_total.billable_hours.to_f.round(2)
