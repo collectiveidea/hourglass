b8fea8e55594a0a1c620f8fb39ee11fe2fb96f68
Chris Gaffney <gaffneyc@gmail.com>
2015-04-13 17:11:40 -0400

    Add an email template for the weekly notifier.
    It's super basic but includes a pie chart for a nice visualization of
    how the time is split. We are using Google's static chart API which has
    a big warning saying that it is deprecated but also saying they have no
    plans to actually kill it. So it will hopefully be good for a while.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,4 +1,7 @@
 class Notifier < ActionMailer::Base
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::NumberHelper
+
   default from: ENV["EMAIL_FROM"]
 
   def weekly_report(user)
@@ -9,4 +12,12 @@ class Notifier < ActionMailer::Base
 
     mail to: user.email
   end
+
+  private
+
+  def pluralize_hours(hours)
+    pluralize(number_with_precision(hours, precision: 1), "hour")
+  end
+
+  helper_method :pluralize_hours
 end
