bcba28fd3f6a42cddcefbdf7a9e0553386d0f158
Steve Richert <steve.richert@gmail.com>
2015-04-23 10:44:37 -0400

    Remove the concepts of a day's month/week number from the code

--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -16,42 +16,6 @@ describe Day do
     end
   end
 
-  context "callbacks" do
-    describe "#set_month_number" do
-      it "sets the month number according to the date" do
-        day = build(:day, date: "2014-12-29") # Monday
-
-        expect {
-          day.save!
-        }.to change {
-          day.month_number
-        }.from(nil).to("201412")
-      end
-    end
-
-    describe "#set_week_number" do
-      it "sets the week-based year week number according to the date" do
-        day = build(:day, date: "2014-12-29") # Monday
-
-        expect {
-          day.save!
-        }.to change {
-          day.week_number
-        }.from(nil).to("201501")
-      end
-
-      it "considers Monday the start of the week" do
-        day = build(:day, date: "2014-12-28") # Sunday
-
-        expect {
-          day.save!
-        }.to change {
-          day.week_number
-        }.from(nil).to("201452")
-      end
-    end
-  end
-
   describe ".last_week" do
     it "returns days from last week (Monday-Sunday)" do
       create(:day, date: 2.weeks.ago.to_date.sunday)
