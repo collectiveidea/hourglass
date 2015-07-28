4d8a9c33c892f4cf76d349631b96cd2524eb8aeb
Steve Richert <steve.richert@gmail.com>
2015-07-28 14:50:12 -0400

    Add Bootstrap assets

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
 require "rails"
+
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -8,7 +9,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 require "open-uri"
