6d16cb917af54ff9f3adb06d0ea1b511858f5762
Jason Roelofs <jasongroelofs@gmail.com>
2016-10-03 13:27:29 -0400

    Move range set outside of the iterator

--- app/interactors/send_team_hours_update.rb
+++ app/interactors/send_team_hours_update.rb
@@ -3,12 +3,11 @@ class SendTeamHoursUpdate
   include HasHarvest
 
   def call
+    from = Date.this_week.first
+    to = Date.this_week.last
+
     Team.active.each do |team|
-      from = Date.this_week.first
-      to = Date.this_week.last
-
       time_entries = harvest.reports.time_by_project(team.project_id, from, to, billable: true)
-
       Notifier.team_hours_update(team, time_entries).deliver_now
     end
   end
