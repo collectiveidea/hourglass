922ff31a0edd963163a345575a855f572d4c816d
Steve Richert <steve.richert@gmail.com>
2017-06-29 15:27:57 -0400

    Avoid collision of two separate basic authentication attempts

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,11 +1,14 @@
 class CalendarsController < ApplicationController
-  before_filter do
-    authenticate_or_request_with_http_basic do |username, _password|
-      ActiveSupport::SecurityUtils.secure_compare(username, ENV["CALENDAR_PASSWORD"])
-    end
-  end
-
   def pto
     render text: Rails.cache.read("pto.ics"), content_type: "text/calendar"
+  end
+
+  private
+
+  # This overrides basic authentication in ApplicationController.
+  def authenticate
+    authenticate_or_request_with_http_basic do |name, _|
+      ActiveSupport::SecurityUtils.variable_size_secure_compare(name, ENV["CALENDAR_PASSWORD"])
+    end
   end
 end
