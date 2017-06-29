ec5b79eb8f58f42e0b6144bf350f6f1a4e8878c5
Steve Richert <steve.richert@gmail.com>
2017-06-29 14:49:00 -0400

    Protect the calendar endpoint(s) behind basic authentication

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,10 @@
 class CalendarsController < ApplicationController
+  before_filter do
+    authenticate_or_request_with_http_basic do |username, _password|
+      ActiveSupport::SecurityUtils.secure_compare(username, ENV["CALENDAR_PASSWORD"])
+    end
+  end
+
   def pto
     render text: Rails.cache.read("pto.ics"), content_type: "text/calendar"
   end
